fc8a6a766cc725191b053d084c1a7e07702ee243
veyanrech <128854610+veyanrech@users.noreply.github.com>
2024-12-14 18:17:37 -0500

    dark images

---
+++ imagecapture/image/image.go
@@ -0,0 +1,46 @@
+package image
+
+import (
+	"image"
+	"os"
+
+	_ "image/jpeg"
+	_ "image/png"
+)
+
+const darkPixelPercentage = 80
+const darkPixelThreshold = 38
+
+// IsImageBlack checks if the image is black
+// i dont close the file, because it should be closed in the calling function
+func IsImageBlack(src *os.File) (r bool, err error) {
+
+	img, _, err := image.Decode(src)
+	// fmt.Println(frmt)
+
+	if err != nil {
+		return false, err
+	}
+
+	darkPixelCount := 0
+	totalPixels := 0
+
+	bounds := img.Bounds()
+
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			totalPixels++
+			r, g, b, _ := img.At(x, y).RGBA()
+			r, g, b = r>>8, g>>8, b>>8
+			if r < darkPixelThreshold && g < darkPixelThreshold && b < darkPixelThreshold {
+				darkPixelCount++
+			}
+		}
+	}
+
+	if darkPixelCount*100/totalPixels > darkPixelPercentage {
+		return true, nil
+	}
+
+	return
+}
---
+++ imagecapture/image/image_test.go
@@ -0,0 +1,48 @@
+package image
+
+import (
+	"os"
+	"testing"
+)
+
+func TestIsImageBlack(t *testing.T) {
+	type args struct {
+		src *os.File
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantR   bool
+		wantErr bool
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				src: nil,
+			},
+			wantR:   false,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var err error
+			tt.args.src, err = os.Open("/Users/mikhailcherniaev/Documents/GitHub/homeWebCamera/imagecapture/image/img.png")
+			if err != nil {
+				t.Errorf("IsImageBlack() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			defer func() {
+				tt.args.src.Close()
+			}()
+			gotR, err := IsImageBlack(tt.args.src)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("IsImageBlack() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotR != tt.wantR {
+				t.Errorf("IsImageBlack() = %v, want %v", gotR, tt.wantR)
+			}
+		})
+	}
+}
